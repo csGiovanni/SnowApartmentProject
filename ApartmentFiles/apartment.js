5b66cc0b3019015d30351d0e8478541ae0274f48
tmd6e <144483373+tmd6e@users.noreply.github.com>
2024-11-18 01:41:22 -0600

    Added streetlight and began work on building object

--- ApartmentFiles/apartment.js
+++ ApartmentFiles/apartment.js
@@ -1,6 +1,6 @@
 var canvas, gl;
 
-var numVertices  = 36;
+var numVertices  = 0;
 var pointsArray = [];
 var colorsArray = [];
 
@@ -24,58 +24,187 @@ var modelViewMatrixLoc, projectionMatrixLoc;
     |/     | /
     C------D/                 */
 var vertices = [
-        vec4(-1,  1,  1, 1.0 ),  // A (0)
-        vec4( 1,  1,  1, 1.0 ),  // B (1)
-        vec4(-1, -1,  1, 1.0 ),  // C (2)
-        vec4( 1, -1,  1, 1.0 ), // D (3)
-        vec4( -1, 1, -1, 1.0 ), // E (4)
-        vec4( 1,  1, -1, 1.0 ), // F (5)
-        vec4( -1,-1, -1, 1.0 ), // G (6)
-        vec4( 1, -1, -1, 1.0 ),  // H (7)
+        //Streetlight Top (Timmy Do)
+        vec4( -1,  0.5,  -0.8, 1.0 ),  // A (0)
+        vec4( 1,  0.5,  -0.8, 1.0 ),  // B (1)
+        vec4( -1,  0.5,  0.8, 1.0 ),  // C (2)
+        vec4( 1,  0.5,  0.8, 1.0 ), // D (3)
+        vec4( -0.8,  0.5,  -1, 1.0 ),  // E (4)
+        vec4( 0.8,  0.5,  -1, 1.0 ),  // F (5)
+        vec4( -0.8,  0.5,  1, 1.0 ),  // G (6)
+        vec4( 0.8,  0.5,  1, 1.0 ), // H (7)
+
+        vec4( -0.7,  0.8,  -0.7, 1.0 ),  // I (8)
+        vec4( 0.7,  0.8,  -0.7, 1.0 ),  // J (9)
+        vec4( -0.7,  0.8,  0.7, 1.0 ),  // K (10)
+        vec4( 0.7,  0.8,  0.7, 1.0 ), // L (11)
+
+        vec4( -1,  -0.5,  -0.8, 1.0 ),  // M (12)
+        vec4( 1,  -0.5,  -0.8, 1.0 ),  // N (13)
+        vec4( -1,  -0.5,  0.8, 1.0 ),  // O (14)
+        vec4( 1,  -0.5,  0.8, 1.0 ), // P (15)
+        vec4( -0.8,  -0.5,  -1, 1.0 ),  // Q (16)
+        vec4( 0.8,  -0.5,  -1, 1.0 ),  // R (17)
+        vec4( -0.8,  -0.5,  1, 1.0 ),  // S (18)
+        vec4( 0.8,  -0.5,  1, 1.0 ), // T (19)
+
+        vec4( -0.7,  -0.8,  -0.7, 1.0 ),  // U (20)
+        vec4( 0.7,  -0.8,  -0.7, 1.0 ),  // V (21)
+        vec4( -0.7,  -0.8,  0.7, 1.0 ),  // W (22)
+        vec4( 0.7,  -0.8,  0.7, 1.0 ), // X (23)
+
+        vec4( -0.8,  0.4,  -0.6, 1.0 ),  // A' (24)
+        vec4( 0.8,  0.4,  -0.6, 1.0 ),  // B' (25)
+        vec4( -0.8,  0.4,  0.6, 1.0 ),  // C' (26)
+        vec4( 0.8,  0.4,  0.6, 1.0 ), // D' (27)
+        vec4( -0.6,  0.4,  -0.8, 1.0 ),  // E' (28)
+        vec4( 0.6,  0.4,  -0.8, 1.0 ),  // F' (29)
+        vec4( -0.6,  0.4,  0.8, 1.0 ),  // G' (30)
+        vec4( 0.6,  0.4,  0.8, 1.0 ), // H' (31)
+
+        vec4( -0.8,  -0.4,  -0.6, 1.0 ),  // M' (32)
+        vec4( 0.8,  -0.4,  -0.6, 1.0 ),  // N' (33)
+        vec4( -0.8,  -0.4,  0.6, 1.0 ),  // O'(34)
+        vec4( 0.8,  -0.4,  0.6, 1.0 ), // P' (35)
+        vec4( -0.6,  -0.4,  -0.8, 1.0 ),  // Q' (36)
+        vec4( 0.6,  -0.4,  -0.8, 1.0 ),  // R' (37)
+        vec4( -0.6,  -0.4,  0.8, 1.0 ),  // S' (38)
+        vec4( 0.6,  -0.4,  0.8, 1.0 ) // T' (39)
+
+
+
     ];
 
 var vertexColors = [
-        vec4( 1.0, 0.0, 0.0, 1.0 ),  // red (0 front)
-        vec4( 0.8, 0.8, 0.2, 1.0 ),  // yellowish-green (1)
-        vec4( 0.0, 1.0, 0.0, 1.0 ),  // green (2)
-        vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue (3 right)
-        vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta (4)
-        vec4( 0.0, 1.0, 1.0, 1.0 ),  // cyan (5 top)
-        vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow (6 left)
+        // Lamp colors
+        vec4(0.5, 0.5, 0.5, 1.0), // Metal
+        vec4(0.4, 0.4, 0.4, 1.0), // Darker Metal
+        vec4(1, 1, 0.25, 1.0), // Light
+
+        // Building colors
+        vec4(0.6, 0.6, 0.6, 1.0), // gray
+        vec4(0.5, 0.5, 0.5, 1.0), // gray (shade)
+        vec4(0.9, 0.9, 0.9, 1.0), // snow
+        vec4(0.7, 0.7, 0.7, 1.0) // snow
     ];
 
 
 // quad uses first index to set color for face
-function quad(a, b, c, d) {
+function quad(a, b, c, d, colorIndex) {
      pointsArray.push(vertices[a]);
-     colorsArray.push(vertexColors[a]);
+     colorsArray.push(vertexColors[colorIndex]);
      pointsArray.push(vertices[b]);
-     colorsArray.push(vertexColors[a]);
+     colorsArray.push(vertexColors[colorIndex]);
      pointsArray.push(vertices[c]);
-     colorsArray.push(vertexColors[a]);
+     colorsArray.push(vertexColors[colorIndex]);
      pointsArray.push(vertices[a]);
-     colorsArray.push(vertexColors[a]);
+     colorsArray.push(vertexColors[colorIndex]);
      pointsArray.push(vertices[c]);
-     colorsArray.push(vertexColors[a]);
+     colorsArray.push(vertexColors[colorIndex]);
      pointsArray.push(vertices[d]);
-     colorsArray.push(vertexColors[a]);
+     colorsArray.push(vertexColors[colorIndex]);
+     numVertices = numVertices + 6;
+}
+
+function tri(a,b,c,colorIndex){
+     pointsArray.push(vertices[a]);
+     colorsArray.push(vertexColors[colorIndex]);
+     pointsArray.push(vertices[b]);
+     colorsArray.push(vertexColors[colorIndex]);
+     pointsArray.push(vertices[c]);
+     colorsArray.push(vertexColors[colorIndex]);
+     numVertices = numVertices + 3;
+}
+function cube(width, height, thickness, locX, locY, locZ, colorIndex){
+    var index = vertices.length;
+    vertices.push(vec4(locX,  locY + height,  locZ + thickness, 1.0 ));  // A (0)
+    vertices.push(vec4(locX + width,  locY + height,  locZ + thickness, 1.0 ));  // B (1)
+    vertices.push(vec4(locX, locY, locZ + thickness, 1.0 ));  // C (2)
+    vertices.push(vec4(locX + width, locY,  locZ + thickness, 1.0 )); // D (3)
+    vertices.push(vec4(locX, locY + height, locZ, 1.0 )); // E (4)
+    vertices.push(vec4(locX + width,  locY + height, locZ, 1.0 )); // F (5)
+    vertices.push(vec4(locX, locY, locZ, 1.0 )); // G (6)
+    vertices.push(vec4(locX + width, locY, locZ, 1.0 ));  // H (7)
+
+    quad(index + 2,index,index + 1,index + 3,colorIndex);
+    quad(index + 3,index + 1,index + 5,index + 7,colorIndex + 1);
+    quad(index + 7,index + 5,index + 4,index + 6,colorIndex);
+    quad(index + 6,index + 4,index,index + 2,colorIndex + 1);
+    quad(index,index + 4,index + 5,index + 1,colorIndex);
+    quad(index + 2,index + 6,index + 7,index + 3,colorIndex + 1);
 }
 
 // Each face is formed with two triangles
-function colorCube() {
-    quad( 0, 1, 3, 2 );  // front(ABDC) red
-    quad( 4, 5, 7, 6 );  // back(EFHG)  magenta
-    quad( 3, 1, 5, 7 );  // right (DBFH) blue
-    quad( 6, 2, 0, 4 );  // left (GCAE) yellow
-    quad( 2, 6, 7, 3 ); // bottom (CGHD) green
-    quad( 5, 4, 0, 1); // top (AEFB) cyan
+function MakeStreetLamp() {
+    //Top
+    tri(0,8,4,1);//AIE
+    quad(4,8,9,5,0);//EIJF
+    tri(5,9,1,1);//FJB
+    quad(1,9,11,3,0);//BJLD
+    tri(3,11,7,1);//DLH
+    quad(10,6,7,11,0);//KGHL
+    tri(2,6,10,1);//CGK
+    quad(2,10,8,0,0);//CKIA
+    quad(8,10,11,9,1);//IKLJ
+
+    //Middle
+    quad(0,24,28,4,0);//AA'E'E
+    quad(4,28,29,5,1);//EE'F'F
+    quad(5,29,25,1,0);//FF'B'B
+    quad(1,25,27,3,1);//BB'D'D
+    quad(3,27,31,7,0);//DD'H'H
+    quad(7,31,30,6,1);//HH'G'G
+    quad(6,30,26,2,0);//GG'C'C
+    quad(2,26,24,0,1);//CC'A'A
+
+    
+    quad(12,32,36,16,0);//MM'Q'Q
+    quad(16,36,37,17,1);//QQ'R'R
+    quad(17,37,33,13,0);//RR'N'N
+    quad(13,33,35,15,1);//NN'P'P 
+    quad(15,35,39,19,0);//PP'T'T
+    quad(19,39,38,18,1);//TT'S'S
+    quad(18,38,34,14,0);//SS'O'O
+    quad(14,34,32,12,1);//OO'M'M
+    
+    quad(24,32,36,28,1);//A'M'Q'E'
+    quad(28,36,37,29,2);//E'Q'R'F'
+    quad(29,37,33,25,1);//F'R'N'B'
+    quad(25,33,35,27,2);//B'N'P'D'
+    quad(27,35,39,31,1);//D'P'T'H'
+    quad(31,39,38,30,2);//H'T'S'G'
+    quad(30,38,34,26,1);//G'S'O'C'
+    quad(26,34,32,24,2);//C'O'M'A'
+
+    //Bottom
+    tri(12,20,16,1);//MUQ
+    quad(16,20,21,17,0);//QUVR
+    tri(17,21,13,1);//RVN
+    quad(13,21,23,15,0);//NVXP
+    tri(15,23,19,1);//PXT
+    quad(22,18,19,23,0);//WSTX
+    tri(14,18,22,1);//OSW
+    quad(14,22,20,12,0);//OWUM
+    quad(20,22,23,21,1);//UWXV
+
+}
+
+function MakeBuilding(xLoc,yLoc,zLoc){
+    // Pillars
+    cube(1,3,1,xLoc,yLoc,zLoc,3);
+    cube(1,3,1,xLoc + 3,yLoc,zLoc,3);
+    cube(1,3,1,xLoc + 6,yLoc,zLoc,3);
+    cube(1,3,1,xLoc + 9,yLoc,zLoc,3);
+    // Top part of building
+    cube(12,3,6,xLoc - 1,yLoc + 3,zLoc - 4,3);
+
 }
 
 // namespace contain all the project information
 var AllInfo = {
 
     // Camera pan control variables.
-    zoomFactor : 8,
+    zoomFactor : 1,
     translateX : 0,
     translateY : 0,
 
@@ -109,7 +238,9 @@ window.onload = function init() {
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
 
-    colorCube();  // created the color cube - point positions and face colors
+    MakeStreetLamp();  // created the color cube - point positions and face colors
+
+    MakeBuilding(1.5,-0.8,-0.5);
 
     var cBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
